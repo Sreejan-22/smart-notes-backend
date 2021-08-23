9fb117fa0a2dae28df280d3ec22ee549ca3a8bef
Sreejan <sreejan147@gmail.com>
2021-08-23 17:43:56 +0530

    feat: added validation to User schema

--- models/User.js
+++ models/User.js
@@ -1,17 +1,19 @@
 const mongoose = require("mongoose");
+const { isEmail } = require("validator");
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, "Please enter an email"],
     unique: true,
     lowercase: true,
+    validate: [isEmail, "Please enter a valid email"],
   },
   password: {
     type: String,
-    required: true,
-    minLength: 6,
-    maxLength: 32,
+    required: [true, "Please enter a password"],
+    minlength: [6, "Minimum password length is 6"],
+    maxlength: [32, "Maximum password length is 32"],
   },
 });
 
